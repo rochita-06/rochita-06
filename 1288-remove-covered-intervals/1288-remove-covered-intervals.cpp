06a31407b801523f0cec79292090256358bdc65e
rochita-06 <rochitagude@gmail.com>
2026-07-06 07:04:27 +0530

    Time: 0 ms (100%), Space: 15.1 MB (60.13%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),[](auto &a,auto &b){
+            if(a[0]==b[0])
+                return a[1]>b[1];
+            return a[0]<b[0];    
+        });
+        int ans=0;
+        int maxRight=-1;
+        for(auto &it:intervals){
+            if(it[1]>maxRight){
+                ans++;
+                maxRight=it[1];
+            }
+        }
+        return ans;
+    }
+};
